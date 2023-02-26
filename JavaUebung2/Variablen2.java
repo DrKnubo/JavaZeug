9d10062ab809b1fa7c174847b7c81cc668b4b7f0
Stefan Schumacher <123940817+DrKnubo@users.noreply.github.com>
2023-02-26 20:46:39 +0100

    Add files via upload

---
+++ JavaUebung2/Variablen2.java
@@ -0,0 +1,26 @@
+public class Variablen2 {
+    public static void main(String[] args){
+        
+char z1 = 'a';
+char z2 = 'b';
+char z3 = 'A';
+char z4 = 0x00a9;
+char z5 = 0x00be;
+
+// Ausgaben
+
+System.out.print("Zeichen 1 = ");
+System.out.println(z1);
+System.out.print("Zeichen 2 = ");
+System.out.println(z2);
+System.out.print("Zeichen 3 = ");
+System.out.println(z3);
+System.out.print("Zeichen 4 = ");
+System.out.println(z4);
+System.out.print("Zeichen 5 = ");
+System.out.println(z5);
+
+
+
+}
+}
